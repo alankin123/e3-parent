99f9ff9e5decbcbeb596d22241f0f168dd68cb47
alankin <460682402@qq.com>
2018-03-01 22:17:19 +0800

    Initial commit

---
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -0,0 +1,10 @@
+package cn.e3mall.service;
+
+import cn.e3mall.pojo.TbItem;
+
+/**
+ * Created by alankin on 2018/2/25.
+ */
+public interface ItemService {
+    public TbItem getItemById(long id);
+}
